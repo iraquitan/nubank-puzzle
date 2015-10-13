43e9f76a3aeaa15457f2d7062facd153226d8792
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2015-10-13 17:59:15 -0300

    Added process to categorical features

--- nubank-puzzle.py
+++ nubank-puzzle.py
@@ -9,36 +9,64 @@
  * Time: 17:07
  * To change this template use File | Settings | File Templates.
 """
+from blaze.expr.datetime import date
 import numpy as np
 import pandas as pd
 from datetime import datetime
 from sklearn.cross_validation import KFold
+from sklearn.feature_extraction import DictVectorizer, FeatureHasher
 from sklearn.metrics import r2_score
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.dummy import DummyRegressor
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 train = pd.read_csv('input/train.csv')
 test = pd.read_csv('input/test.csv')
 
 # Pre-process step
-num_train = train.ix[:, train.applymap(np.isreal).all(axis=0)]
+num_train = train.ix[:, train.applymap(np.isreal).all(axis=0)]  # Get numerical features
+cat_train = train.ix[:, np.invert(train.applymap(np.isreal).all(axis=0))]  # Get categorical features
+
+categorical_type = 'dictvectorizer'
+cat_dict = (dict(cat_train.ix[x]) for x in range(cat_train.shape[0]))  # Categorical data generator faster then pandas
+# cat_dict = cat_train.to_dict(orient='records')  # Categorical data dict
+if categorical_type == 'dictvectorizer':
+    vec = DictVectorizer()
+    cat_data = vec.fit_transform(cat_dict).toarray()
+elif categorical_type == 'featurehasher':
+    feat_hash = FeatureHasher()
+    hasher = FeatureHasher(input_type='string', n_features=2**8)
+    cat_data = hasher.transform(cat_dict)
+elif categorical_type == 'onehotencoder':
+    le_data = np.empty(cat_train.shape)
+    for col in range(cat_train.shape[1]):
+        le = LabelEncoder()
+        le_data[:, col] = le.fit_transform(cat_train.ix[:, col])
+    enc = OneHotEncoder()
+    cat_data = enc.fit_transform(le_data).toarray()
+
+
 np_data = np.array(num_train)
 ids = np_data[:, -2]
-x_tr = np_data[:, :-2]
+# x_tr = np_data[:, :-2]
+x_tr = cat_data
 y_tr = np_data[:, -1]
 
+# TODO get random subsample
 subsample_rate = 2
 x_tr = x_tr[::subsample_rate, :]
 y_tr = y_tr[::subsample_rate]
 
 # Training step
 r2scores = []
-kf = KFold(x_tr.shape[0], n_folds=10)
+dummy_r2scores = []
+n_etim = 50
+n_fs = 10
+kf = KFold(x_tr.shape[0], n_folds=n_fs)
 train_type = 'regressor'
 if train_type == 'regressor':
-    mdl = RandomForestRegressor(n_estimators=10, n_jobs=-1)
-elif train_type == 'classifier':
-    mdl = RandomForestClassifier(n_estimators=10, n_jobs=-1)
+    mdl = RandomForestRegressor(n_estimators=n_etim, n_jobs=-1)
+    dummy_dml = DummyRegressor(strategy='mean')
 else:
     raise Exception('Train type not defined!')
 
@@ -47,17 +75,19 @@ fold = 1
 for train, test in kf:
     fold_start_time = datetime.now()
     mdl.fit(x_tr[train, :], y_tr[train])
+    dummy_dml.fit(x_tr[train, :], y_tr[train])
     if train_type == 'regressor':
         y_pred = mdl.predict(x_tr[test, :])
-    elif train_type == 'classifier':
-        y_pred = mdl.predict_proba(x_tr[test, :])
+        dummy_y_pred = dummy_dml.predict(x_tr[test, :])
     r2scores.append(r2_score(y_tr[test], y_pred))
+    dummy_r2scores.append(r2_score(y_tr[test], dummy_y_pred))
     fold_elapsed_time = datetime.now() - fold_start_time
     print('Fold {} finished in {}'.format(fold, fold_elapsed_time))
     fold += 1
 elapsed_time = datetime.now() - start_time
 print('{}-fold process finished in {}'.format(fold, elapsed_time))
 print('R2 scores: {}\nR2 mean score: {}'.format(r2scores, np.mean(r2scores)))
+print('Dummy R2 scores: {}\nDummy R2 mean score: {}'.format(dummy_r2scores, np.mean(dummy_r2scores)))
 
 # Testing step
 
